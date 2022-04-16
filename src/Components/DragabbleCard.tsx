0b2fb6a6854584d780d769b936fcf1646f0e323a
chominho14 <chominho14@naver.com>
2022-04-16 16:19:13 +0900

    v0.10(Drag and Drop - Recoil Reordering - React memo로 최적화)

--- src/Components/DragabbleCard.tsx
+++ src/Components/DragabbleCard.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
@@ -29,4 +30,4 @@ function DraggabbleCard({ toDo, index }: IDragabbleCardProps) {
   );
 }
 
-export default DraggabbleCard;
+export default React.memo(DraggabbleCard);
